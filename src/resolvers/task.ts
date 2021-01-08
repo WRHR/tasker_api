e9804f162cb6d487576f0b9ad2ec6d5ab7459fa8
WRHR <wrhreeves@gmail.com>
2021-01-07 20:20:21 -0700

    Create TaskResolver class

---
+++ src/resolvers/task.ts
@@ -0,0 +1,9 @@
+import { ObjectType } from 'type-graphql'
+import {MyContext} from '../types'
+import {Task} from '../entities/Task'
+import { } from 'graphql'
+
+@ObjectType()
+export class TaskResolver{
+  
+}
