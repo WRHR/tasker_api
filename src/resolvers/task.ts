897ed012cb303cabc76f2ef479afeadcbea87b27
WRHR <wrhreeves@gmail.com>
2021-01-07 22:15:11 -0700

    Edit createTask mutation

--- src/resolvers/task.ts
+++ src/resolvers/task.ts
@@ -1,7 +1,20 @@
-import { Arg, Field, Mutation, ObjectType } from 'type-graphql'
-import {MyContext} from '../types'
-import {Task} from '../entities/Task'
-import { } from 'graphql'
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType } from "type-graphql";
+import { MyContext } from "../types";
+import { Task } from "../entities/Task";
+import {} from "graphql";
+import { getConnection } from "typeorm";
+
+@InputType()
+class TaskOptions {
+  @Field()
+  title: string;
+
+  @Field()
+  text: string;
+
+  @Field()
+  category: string;
+}
 
 @ObjectType()
 class FieldError {
@@ -13,15 +26,49 @@ class FieldError {
 
 @ObjectType()
 class TaskResponse {
-  @Field(()=>[FieldError], {nullable:true})
-  errors?:FieldError[]
-  
-  @Field(()=>Task, {nullable:true})
-  task?:Task
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => Task, { nullable: true })
+  task?: Task;
 }
 
 @ObjectType()
-export class TaskResolver{
-  @Mutation(()=>TaskResponse)
-  async createTask(@Arg('options') options:)
+export class TaskResolver {
+  @Mutation(() => TaskResponse)
+  async createTask(
+    @Arg("options") options: TaskOptions,
+    @Ctx() { req }: MyContext
+  ): Promise<TaskResponse> {
+    let task;
+
+    try {
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(Task)
+        .values({
+          title: options.title,
+          text: options.text,
+          category: options.category,
+          creatorId: req.session.userId,
+        })
+        .returning("*")
+        .execute();
+      console.log("task result", result);
+      task = result.raw[0];
+    } catch (err) {
+      if (err) {
+        return {
+          errors: [
+            {
+              field: "task",
+              message: "cannot create task",
+            },
+          ],
+        };
+      }
+    }
+    return { task };
+  }
 }
