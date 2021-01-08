29a4f358c680f245ec6b01c5ad392f1bad4c3fbb
WRHR <wrhreeves@gmail.com>
2021-01-07 20:32:57 -0700

    Edit task resolver

--- src/resolvers/task.ts
+++ src/resolvers/task.ts
@@ -1,9 +1,27 @@
-import { ObjectType } from 'type-graphql'
+import { Arg, Field, Mutation, ObjectType } from 'type-graphql'
 import {MyContext} from '../types'
 import {Task} from '../entities/Task'
 import { } from 'graphql'
 
 @ObjectType()
-export class TaskResolver{
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class TaskResponse {
+  @Field(()=>[FieldError], {nullable:true})
+  errors?:FieldError[]
   
+  @Field(()=>Task, {nullable:true})
+  task?:Task
+}
+
+@ObjectType()
+export class TaskResolver{
+  @Mutation(()=>TaskResponse)
+  async createTask(@Arg('options') options:)
 }
