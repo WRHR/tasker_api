ec3908fa6515b86c4ff995039517bfaa3d2b88a6
WRHR <wrhreeves@gmail.com>
2021-01-10 22:29:20 -0700

    Add completeTask Mutation

--- src/entities/CompletedTask.ts
+++ src/entities/CompletedTask.ts
@@ -17,11 +17,11 @@ export class CompletedTask {
 
   @Field()
   @OneToOne(() => Task, (task) => task.id)
-  taskId: Task;
+  taskId!: Task;
 
   @Field()
   @ManyToOne(() => User, (user) => user.completedTasks)
-  userId: User;
+  userId!: User;
 
   @Field(() => String)
   @CreateDateColumn()
--- src/resolvers/completeTask.ts
+++ src/resolvers/completeTask.ts
@@ -13,10 +13,58 @@ import {} from "graphql";
 import { getConnection } from "typeorm";
 import { CompletedTask } from "src/entities/CompletedTask";
 
+ObjectType()
+class FieldError {
+  @Field()
+  field: string
+  @Field()
+  message:string
+}
+
+@ObjectType()
+class CompletedTaskRes {
+  @Field(()=> [FieldError], {nullable:true})
+  errors?: FieldError[]
+
+  @Field(()=>CompletedTask, {nullable:true})
+  compTask?:CompletedTask 
+}
+
 @ObjectType()
 export class CompleteTaskResolver {
   @Query(() => [CompletedTask])
   async myCompletedTasks(@Ctx() { req }: MyContext): Promise<CompletedTask[]> {
     return await CompletedTask.find({ userId: req.session.id });
   }
+
+  @Mutation (()=>CompletedTaskRes)
+  async completeTask(@Arg('taskNum') taskNum:number,
+  @Ctx() {req}:MyContext): Promise<CompletedTaskRes>{
+    let ct 
+
+    try{
+      const result = await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(CompletedTask)
+      .values({
+        taskId: taskNum.toString(),
+        userId: req.session.userId?.toString()
+      })
+      .returning('*')
+      .execute()
+      ct = result.raw[0]
+    } catch (err) {
+      return {
+        errors: [
+          {
+            field:'completed task',
+            message:'An error occured'
+          }
+        ]
+      }
+    }
+    return {ct}
+  }
+
 }
